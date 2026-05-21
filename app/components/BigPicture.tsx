1d10e7fa62111679f93ff7db96fc1683ae067440
Nyamochir99 <nyamochir.lkhamkhvv@gmail.com>
2026-05-21 12:29:01 +0800

    header fixed bolgow

--- app/components/BigPicture.tsx
+++ app/components/BigPicture.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const BigPicture = () => {
   return (
     <div
-      className="w-full h-142.5 bg-cover bg-center bg-no-repeat"
+      className="w-full h-142.5 bg-cover bg-center bg-no-repeat mt-17"
       style={{
         backgroundImage: "url(/icons/bigpic.svg)",
       }}
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ import { LoggedIn } from "./LoggedIn";
 
 export const Header = ({ user }: { user: boolean }) => {
   return (
-    <div className="flex justify-center w-full bg-[#18181B]">
+    <div className="flex justify-center w-full bg-[#18181B] fixed top-0">
       <div className="w-7xl py-3 flex justify-between items-center">
         <Logo isVertical={false} />
         {user ? <LoggedIn /> : <Signup />}
