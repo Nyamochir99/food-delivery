fe74890e2ba95595f255b417816b5ae75e45d7ea
Nyamochir99 <nyamochir.lkhamkhvv@gmail.com>
2026-05-25 12:20:55 +0800

    signuo step nemeh

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="flex w-full h-full justify-center items-center p-5">
-      <div className="w-7xl flex gap-12">
+    <div className="flex w-full h-screen justify-center items-center p-5">
+      <div className="w-7xl flex gap-12 items-center h-full">
         <div className="flex flex-col gap-6 w-104">
           <button className="flex items-center justify-center h-9 w-9 border border-[#E4E4E7] rounded-md bg-white">
             <Image src="/icons/back.svg" alt="back" width={16} height={16} />
@@ -18,11 +18,21 @@ export default function LoginPage() {
           </div>
           <input
             type="text"
-            className="w-full h-9 border border-[#E4E4E7] rounded-md placeholder:text-sm placeholder:text-[#71717A] placeholder:font-normal"
+            className="w-full h-9 outline-none px-3 border border-[#E4E4E7] rounded-md placeholder:text-sm placeholder:text-[#71717A] placeholder:font-normal"
             placeholder="Enter your email address"
           />
-          <button>Let&apos;s Go</button>
+          <button className="h-9 2-full rounded-md cursor-pointer text-sm font-medium text-[#FAFAFA] bg-[#18181B] disabled:opacity-20">
+            Let&apos;s Go
+          </button>
+          <div className="flex justify-center gap-3 text-base font-normal">
+            <span className="text-[#71717A]">Already have an account?</span>
+            <span className="text-[#2563EB] cursor-pointer">Log in </span>
+          </div>
         </div>
+        <div
+          className="w-214 h-226 rounded-3xl overflow-hidden bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: "url(/icons/bg.svg)" }}
+        ></div>
       </div>
     </div>
   );
